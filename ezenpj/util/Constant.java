6ff037f7da40cd753da2f3608ac6d7b25a927284
kim-yoojin <101693293+kim-yoojin@users.noreply.github.com>
2022-03-16 15:16:45 +0900

    Add files via upload
    연습

---
+++ ezenpj/util/Constant.java
@@ -0,0 +1,11 @@
+package com.kook.ezenpj.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.kook.ezenpj.dao.MiniDao;
+
+public class Constant {
+	public static MiniDao mdao;
+	public static BCryptPasswordEncoder passwordEncoder;
+	public static String username;
+}
